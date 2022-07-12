828360fa2e07c905118c3d0a20966e74509d61a0
Sofia Aguilera <aguilerasofia.dv@gmail.com>
2022-07-12 20:58:54 -0300

    eliminar carpetas innecesarias y acomodar codigo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+//componente principal 
+
 import './App.css';
+import NavBar from './components/NavBar/NavBar'
+
 
 function App() {
+  const styleApp = {padding: "10px 20px", marginTop: 10}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //JSX
+    <div className="container" style= {styleApp}>
+      <NavBar></NavBar>
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return(
+        //fragment, etiqueta fantasma: <> </>
+        <> 
+        <div>
+            <h1>Logo</h1>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Sobre Mi</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+
+export default NavBar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
